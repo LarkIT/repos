bcf3a109b5a9090ec80d0ab1c7d0cc89b1a27ddc
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-10-31 12:43:00 -0600

    troubleshooting

--- manifests/openvpn.pp
+++ manifests/openvpn.pp
@@ -3,10 +3,10 @@
 # Purpose: Define the repo for pulp
 #
 class repos::openvpn {
-  yumrepo { 'openvpn-as':
-    descr    => 'OpenVPN Access Server',
-    baseurl  => "${profile::pulp_client::server_protocol}://${profile::pulp_client::server_name}/pulp/repos/openvpn/el${::operatingsystemmajrelease}/${::architecture}/",
-    gpgcheck => '0',
-    enabled  => '1',
-  }
+#  yumrepo { 'openvpn-as':
+#    descr    => 'OpenVPN Access Server',
+#    baseurl  => "${profile::pulp_client::server_protocol}://${profile::pulp_client::server_name}/pulp/repos/openvpn/el${::operatingsystemmajrelease}/${::architecture}/",
+#    gpgcheck => '0',
+#    enabled  => '1',
+#  }
 }
