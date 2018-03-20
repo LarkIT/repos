3191d4aa51c6af02d474ef067dab4e24cf7c9365
Costa Argiris <costa.argiris@lark-it.com>
2018-03-20 11:01:37 -0600

    Updated Sensu repo to lookup data
    DEVOPS-642

--- manifests/sensu.pp
+++ manifests/sensu.pp
@@ -3,9 +3,13 @@
 # Purpose: Define the repo for sensu
 #
 class repos::sensu {
+
+  $server_name     = lookup('profile::pulp_client::server_name', String, 'first', 'pulp.lark-it.com')
+  $server_protocol = lookup('profile::pulp_client::server_protocol', String, 'first', 'http')
+
   yum::repo { 'sensu-official':
     descr    => 'Sensu',
-    baseurl  => "${profile::pulp_client::server_protocol}://${profile::pulp_client::server_name}/pulp/repos/sensu/${::architecture}",
+    baseurl  => "$server_protocol://$server_name/pulp/repos/sensu/${::architecture}",
     gpgcheck => '0',
     enabled  => '1',
   }
