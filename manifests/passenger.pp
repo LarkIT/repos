8bde670d4949c384ab891635f6c0cb66e05b355f
Costa Arigirs <costa.argiris@lark-it.com>
2018-05-02 10:27:29 -0600

    Added lookups to the passenger repo for dynamic config
    DEVOPS-651

--- manifests/passenger.pp
+++ manifests/passenger.pp
@@ -4,10 +4,13 @@
 #
 class repos::passenger {
 
+  $server_name     = lookup('profile::pulp_client::server_name', String, 'first', 'pulp.lark-it.com')
+  $server_protocol = lookup('profile::pulp_client::server_protocol', String, 'first', 'http')
+
  if lookup('profile::pulp_client::rel_slash', Boolean, 'first', true) {
-  $baseurl = "${profile::pulp_client::server_protocol}://${profile::pulp_client::server_name}/pulp/repos/passenger/el/${::operatingsystemmajrelease}/${::architecture}"
+  $baseurl = "$server_protocol://$server_name/pulp/repos/passenger/el/${::operatingsystemmajrelease}/${::architecture}"
  } else {
-  $baseurl = "${profile::pulp_client::server_protocol}://${profile::pulp_client::server_name}/pulp/repos/passenger/el${::operatingsystemmajrelease}/${::architecture}"
+  $baseurl = "$server_protocol://$server_name/pulp/repos/passenger/el${::operatingsystemmajrelease}/${::architecture}"
  }
 
   yumrepo { "passenger-centos${::operatingsystemmajrelease}-${::architecture}":
