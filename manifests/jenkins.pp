eee7db8295866c8079a6ba2c4cc860aed221c191
Costa Arigirs <costa.argiris@lark-it.com>
2018-07-09 18:11:25 -0600

    Updated jenkins repo class
    DEVOPS-544

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -3,11 +3,16 @@
 # Purpose: Define the Jenkins Stable repo for CentOS.
 #
 class repos::jenkins {
+
+  $server_name     = lookup('profile::pulp_client::server_name', String, 'first', 'pulp.lark-it.com')
+  $server_protocol = lookup('profile::pulp_client::server_protocol', String, 'first', 'http')
+  $gpg_uri = lookup('profile::pulp_client::gpg_uri', String, 'first', 'static')
+
   yum::repo { "centos${::operatingsystemmajrelease}-${::architecture}-jenkins":
     descr    => "CentOS ${::operatingsystemmajrelease} ${::architecture} - Jenkins",
-    baseurl  => "${profile::pulp_client::server_protocol}://${profile::pulp_client::server_name}/pulp/repos/jenkins/",
+    baseurl  => "$server_protocol://$server_name/pulp/repos/jenkins/",
     gpgcheck => '1',
     enabled  => '1',
-    gpgkey   => "${profile::pulp_client::server_protocol}://${profile::pulp_client::server_name}/pulp/${profile::pulp_client::gpg_uri}/jenkins-ci.org.key",
+    gpgkey   => "$server_protocol://$server_name/pulp/$gpg_uri/jenkins-ci.org.key",
   }
 }
