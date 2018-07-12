6d43963d10512a4e30bc2fce8eed4b08e9ae1548
Costa Arigirs <costa.argiris@lark-it.com>
2018-07-12 11:38:34 -0600

    Updated openvpn_as repo to use lookups for pulp client data
    DEVOPS-683

--- manifests/openvpn_as.pp
+++ manifests/openvpn_as.pp
@@ -3,9 +3,13 @@
 # Purpose: Define the repo for openvpn_as
 #
 class repos::openvpn_as {
+
+  $server_name     = lookup('profile::pulp_client::server_name', String, 'first', 'pulp.lark-it.com')
+  $server_protocol = lookup('profile::pulp_client::server_protocol', String, 'first', 'http')
+
   yumrepo { 'openvpn-as':
     descr    => 'OpenVPN Access Server',
-    baseurl  => "${profile::pulp_client::server_protocol}://${profile::pulp_client::server_name}/pulp/repos/openvpn/el${::operatingsystemmajrelease}/${::architecture}/",
+    baseurl  => "$server_protocol://$server_name/pulp/repos/openvpn/el${::operatingsystemmajrelease}/${::architecture}/",
     gpgcheck => '0',
     enabled  => '1',
   }
