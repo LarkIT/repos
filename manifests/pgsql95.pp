9a62bfaf83e6084d1420099c1fca16c9cf529107
Ethan Claassen <ethan.claassen@lark-it.com>
2020-01-02 12:45:09 -0700

    Update postgresql 9.5 repo URL

--- manifests/pgsql95.pp
+++ manifests/pgsql95.pp
@@ -5,7 +5,7 @@
 class repos::pgsql95 {
   yum::repo { "centos${::operatingsystemmajrelease}-${::architecture}-pgsql95":
     descr    => "CentOS ${::operatingsystemmajrelease} ${::architecture} - pgsql95",
-    baseurl  => "${profile::pulp_client::server_protocol}://${profile::pulp_client::server_name}/pulp/repos/postgresql/9.5/rhel-${::operatingsystemmajrelease}-${::architecture}",
+    baseurl  => "${profile::pulp_client::server_protocol}://${profile::pulp_client::server_name}/pulp/repos/postgres/9.5/rhel-${::operatingsystemmajrelease}-${::architecture}",
     gpgcheck => '1',
     enabled  => '1',
     gpgkey   => "${profile::pulp_client::server_protocol}://${profile::pulp_client::server_name}/pulp/${profile::pulp_client::gpg_uri}/RPM-GPG-KEY-PGDG-92",
